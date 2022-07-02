72b62014d351b94fbc5b2d74bc595c6f1d81ccab
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-07-02 10:17:21 -0400

    added 5 features

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,19 +6,40 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String education;
+    private boolean canSwim;
+    private String address;
+    private int numOfPets;
+    private boolean ownPhone;
 
     public Person() {
         this.name = "";
         this.age = Integer.MAX_VALUE;
+        this.education = "";
+        this.canSwim = false;
+        this.address = "";
+        this.numOfPets = Integer.MAX_VALUE;
+        this.ownPhone = false;
+
     }
 
     public Person(int age) {
         this.age = 5;
+        this.numOfPets = 3;
     }
 
-    public Person(String name) {
-        this.name = "Leon";
+    public Person(boolean canSwim) {
+        this.canSwim = true;
+        this.ownPhone = true;
     }
+
+    public Person(String education) {
+        this.name = "Leon";
+        this.education = "Associates";
+        this.address = "1234 north philly";
+    }
+
+
 
     public Person(String name, int age) {
         this.name = "Leon";
@@ -33,6 +54,26 @@ public class Person {
         this.age = age;
     }
 
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public void setCanSwim(boolean canSwim){
+        this.canSwim = canSwim;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNumOfPets(int numOfPets){
+        this.numOfPets = numOfPets;
+    }
+
+    public void setOwnPhone(boolean ownPhone){
+        this.ownPhone = ownPhone;
+    }
+
     public String getName() {
         return this.name;
     }
@@ -40,4 +81,10 @@ public class Person {
     public Integer getAge() {
         return this.age;
     }
+
+    public String getEducation() { return this.education; }
+    public String getAddress() { return this.address; }
+    public boolean getCanSwim() { return this.canSwim; }
+    public boolean getOwnPhone() { return this.ownPhone; }
+    public int getNumOfPets() { return this.numOfPets; }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,6 +12,11 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        String expectedEducation = "";
+        boolean expectedSwim = false;
+        String expectedAddress = "";
+        Integer expectedNumOfPets = Integer.MAX_VALUE;
+        boolean expectedOwnPhone = false;
 
         // When
         Person person = new Person();
@@ -19,9 +24,19 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        String actualEducation = person.getEducation();
+        boolean actualSwim = person.getCanSwim();
+        String actualAddress = person.getAddress();
+        Integer actualNumOfPet = person.getNumOfPets();
+        boolean actualOwnPhone = person.getOwnPhone();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedEducation, actualEducation);
+        Assert.assertEquals(expectedSwim, actualSwim);
+        Assert.assertEquals(expectedAddress, actualAddress);
+        Assert.assertEquals(expectedNumOfPets, actualNumOfPet);
+        Assert.assertEquals(expectedOwnPhone, actualOwnPhone);
     }
 
     @Test
@@ -93,6 +108,68 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithEducation() {
+        // Given
+        String expected = "Associates";
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        String actual = person.getEducation();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithAddress() {
+        // Given
+        String expected = "1234 north philly";
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        String actual = person.getAddress();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConstructorWithCanSwim() {
+        // Given
+        boolean expected = true;
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        boolean actual = person.getCanSwim();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConstructorWithOwnPhone() {
+        // Given
+        boolean expected = true;
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        boolean actual = person.getOwnPhone();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConstructorWithNumOfPets() {
+        // Given
+        int expected = 3;
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        int actual = person.getNumOfPets();
         Assert.assertEquals(expected, actual);
     }
 }
